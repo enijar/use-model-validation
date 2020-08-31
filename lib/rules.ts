043cecea1be3da101c2fed9316a5f6c1b90e2075
Enijar <TheEnijar@gmail.com>
2020-08-31 10:18:05 +0100

    feat: evaluate rule args as optional function before testing values

--- lib/rules.ts
+++ lib/rules.ts
@@ -7,18 +7,29 @@ const R: ruleMethodsType = {
     message: formatMessage(message),
   }),
   min(min, message = "Too small, min: :min") {
+    if (typeof min === "function") {
+      min = min();
+    }
+    console.log({ min });
     return (normal) => ({
       pass: !length(normal, [1]) ? true : length(normal, [min, Infinity]),
       message: formatMessage(message, { min }),
     });
   },
   max(max, message = "Too large, max: :max") {
+    if (typeof max === "function") {
+      max = max();
+    }
     return (normal) => ({
       pass: length(normal, [0, max]),
       message: formatMessage(message, { max }),
     });
   },
-  between([min, max], message = "Out of range, must be between :min and :max") {
+  between(range, message = "Out of range, must be between :min and :max") {
+    if (typeof range === "function") {
+      range = range();
+    }
+    const [min, max] = range;
     return (normal) => ({
       pass: !length(normal, [1]) ? true : length(normal, [min, max]),
       message: formatMessage(message, { min, max }),
